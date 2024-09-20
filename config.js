77bec3c4a5c9a2fba6a06ac4d78f7f63696460f4
cobr23 <contactprasad32@gmail.com>
2024-09-20 17:16:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,11 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "put sesion_id",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/4y72vl.jpg",
-ALIVE_MSG: process.env.ALIVE_IMG || "👋 Hello *${pushname}*\n🌝👋 HELLO, I AM HANSAMAL-MD I AM ALIVE NOW !",
-SUDO_NB: process.env.SUDO_NB || "",
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-MODE: process.env.MODE || "public",
-AUTO_VOICE:"true"
+MONGODB: process.env.MONGODB || ""
 };
 
